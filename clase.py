c2dd42f1bc6c4ba6daac12f782183b7a38b7ea00
Santiago Mesa <thiagomesa21@gmail.com>
2025-01-17 19:15:16 -0500

    segundo punto de la clase.

--- clase.py
+++ clase.py
@@ -29,3 +29,21 @@ for i in range(5):
     valor = int(input(f"ingrese un numero para la posicion {i}: "))
     vector.append(valor) #.append es una funcion interna de python que recibe como parametro lo que se va a agregar a un arreglo y lo coloca en la ultima posicion disponible.
 print(f"Arreglo ingresado: {vector}")
+
+
+# Recorrer y modificar valores dentro de un vetor / arreglo / Array
+
+vector2 = [10, 20, 30, 40, 50]
+#lo recorremos o iteramos, haciendo alguna operacion en cada uno de los elementos que contiene el arreglo. la mejor herramienta para hacerlo, es un ciclo for.
+print("Elementos originales")
+for i in range(len(vector2)):#len cuenta cuantos elementos tiene el vector y se usa para hacer algunas operaciones basicas.
+    print(f"indice {i}: {vector2[i]}")#[n] hace refencia a la posicion donde esta cada elemento del arreglo.
+
+#modificar cada elemento multiplicandolo por 2
+for i in range(len(vector2)):
+    vector2[i] *= 2 #se modifica un elemento llamando a su posicion en el indice e igualando al nuevo valor que se le quiere asignar.
+
+#volvemos a imprimirlo
+print("Vector luego de ser modificado: ")
+for i in range(len(vector2)):
+    print(f"Indice {i}: {vector2[i]}")
